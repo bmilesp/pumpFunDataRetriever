5cd7ab7543319a263314a17081eb1a9382e9dc93
bmilesp <bmilesp@gmail.com>
2025-01-17 15:24:52 -0500

    reply fetched

--- docker-pump-fun-scraper/scrape_websocket.js
+++ docker-pump-fun-scraper/scrape_websocket.js
@@ -1,6 +1,11 @@
 const fs = require("fs");
 const { chromium } = require("playwright");
 
+function bufferToString(buffer) {
+  const decoder = new TextDecoder(); // Assuming UTF-8 encoding
+  return decoder.decode(buffer); 
+}
+
 (async () => {
   // Launch browser
   const browser = await chromium.launch({ headless: true }); // Set to true to hide the browser
@@ -10,7 +15,7 @@ const { chromium } = require("playwright");
   // Output files for categorized messages
   const tradeCreatedFile = "/usr/src/app/output/tradeCreated.txt";
   const newCoinCreatedFile = "/usr/src/app/output/newCoinCreated.txt";
-
+  const otherFile = "/usr/src/app/output/other.txt";
   // Ensure output directory exists
   const outputDirectory = "/usr/src/app/output";
   if (!fs.existsSync(outputDirectory)) {
@@ -26,26 +31,61 @@ const { chromium } = require("playwright");
       try {
         // Check for "tradeCreated" and "newCoinCreated"
         var input = frame.payload;
-        console.log("INPUT: ", input);
+        //console.log("INPUT: ", input);
         if (frame.payload.includes("tradeCreated")) {
           //console.log(`tradeCreated message: ${frame.payload}`);
           var jsonSplit = input.split('[', 2);
-          console.log('payload: ',jsonSplit);
           var payload = JSON.parse("["+jsonSplit[1]);
-          console.log('payload: ',jsonSplit);
-          fs.appendFileSync(tradeCreatedFile, JSON.stringify(payload, null, 2) + "\n");
+          //console.log(payload[1]);
+          fs.appendFileSync(tradeCreatedFile, JSON.stringify(payload[1], null, 2) + "\n");
         } else if (frame.payload.includes("newCoinCreated")) {
           //console.log(`newCoinCreated message: ${frame.payload}`);
-          var jsonSplit = input.split('{', 2);
+          var jsonSplit =  bufferToString(input).split('{', 2);
           var payload = JSON.parse("{"+jsonSplit[1]);
-          console.log('payload: ',jsonSplit);
+          //console.log('payload: ',jsonSplit);
           fs.appendFileSync(newCoinCreatedFile, JSON.stringify(payload, null, 2) + "\n");
+
+          if (payload.mint) {
+            console.log(`Sending mint to fetch-replies: ${payload.mint}`);
+
+            fetch(fetcherEndpoint, {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({ mint: payload.mint }),
+            });
+          }
+       
+
+        } else if (frame.payload.includes("Buffer")) {
+          console.log(`Buffer message: ${frame.payload}`);
+          var payload = JSON.parse(input);
+          var inputString =  bufferToString(payload["data"]);
+          console.log(inputString)
+          fs.appendFileSync(otherFile, JSON.stringify(inputString, null, 2) + "\n");
+        } else{
+          console.log("INPUT: ",  input);
+          //var stringInput = JSON.parse(input)
+          //fs.appendFileSync(otherFile, input + "\n");
+  
+          if (input.includes("{")) {
+            console.log("JSON: ",  input);
+            var jsonSplit =  bufferToString(input).split('{', 2);
+            var payload = JSON.parse("{"+jsonSplit[1]);
+            console.log("PAYLOAD: ",  payload);
+            fs.appendFileSync(otherFile, JSON.stringify( payload, null, 2) + "\n");
+          }
         }
       } catch (error) {
         console.log(error)  
         // Handle non-JSON payloads or errors gracefully
         //console.error("Failed to parse WebSocket message:", frame.payload);
       }
+    });
+
+    ws.on("framesent", (frame) => {
+      console.log(`Sent: ${frame.payload}`);
+      // Save message to file
+     //fs.appendFileSync(outputFile, `Sent: ${frame.payload}\n`);
     });
   });
 
